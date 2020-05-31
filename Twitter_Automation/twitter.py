5fb941eeb159e53db2c09ca24ff86922139f7da7
tech force <misbah4064@gmail.com>
2020-05-30 20:20:06 -0500

    send tweets through python

---
+++ Twitter_Automation/twitter.py
@@ -0,0 +1,10 @@
+import tweepy
+from keys import *
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+api.update_status('sending an automated tweet from python')
+
+
